cbd3475458c5ed9455b19e02e3318bc101383da3
Арцыбашев Виталий <arcvetal@gmail.com>
2021-03-19 11:35:57 +0300

    Markups

--- src/App.vue
+++ src/App.vue
@@ -70,7 +70,7 @@
           </div>
         </div>
         <button
-          v-on:click="add"
+          @click="add"
           type="button"
           class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
         >
@@ -94,11 +94,13 @@
       <hr class="w-full border-t border-gray-600 my-4" />
       <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
         <div
+          v-for="item in tickers"
+          v-bind:key="item"
           class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
         >
           <div class="px-4 py-5 sm:p-6 text-center">
             <dt class="text-sm font-medium text-gray-500 truncate">
-              WTF - USD
+              {{ item }}
             </dt>
             <dd class="mt-1 text-3xl font-semibold text-gray-900">
               1.11
